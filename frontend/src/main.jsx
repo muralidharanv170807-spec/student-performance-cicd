0872dbf35558fb172685d2c638c7f84d796db060
Muralidharan V <muralidharan.v170807@gmail.com>
2026-08-12 23:04:17 +0530

    Connect frontend to deployed API

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,7 +29,9 @@ function App() {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch(
+  "https://student-performance-cicd.onrender.com/predict",
+  {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
